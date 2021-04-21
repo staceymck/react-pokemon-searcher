ae1d629833a1c2f27247b58566c7bd08ebc7dff3
Stacey McKnight <mcknight.stm@gmail.com>
2021-04-21 14:13:59 -0500

    Done.

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -6,7 +6,10 @@ class PokemonCollection extends React.Component {
   render() {
     return (
       <Card.Group itemsPerRow={6}>
-        <h1>Hello From Pokemon Collection</h1>
+        {this.props.pokemon.map(p => {
+            return <PokemonCard key={p.id} pokemon={p} toggleImage={this.props.toggleImage} />
+          })
+        }
       </Card.Group>
     )
   }
